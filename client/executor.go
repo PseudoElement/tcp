0a2526bf0ce01446a01f85dd2c7f8c01fc42b5ee
pseudoelement <bubamainer@mail.ru>
2026-01-11 00:19:14 +0300

    add powershell support for commands

--- client/executor.go
+++ client/executor.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"os/exec"
+	"runtime"
 	"strings"
 )
 
 func execute(msg string) (outMsg string, err error) {
 	command := strings.TrimPrefix(msg, "$ ")
 
-	// Use shell to interpret pipes, redirects, etc.
-	cmd := exec.Command("sh", "-c", command)
+	var cmd *exec.Cmd
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("powershell", "-Command", command)
+	} else {
+		cmd = exec.Command("sh", "-c", command)
+	}
+
 	out, err := cmd.Output()
 	outMsg = string(out)
 
